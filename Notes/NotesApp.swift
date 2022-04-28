eb45bfb1c5c43314f522f106edce3664bb316c59
TsaybelSA <tsaybelsa@gmail.com>
2022-04-28 19:11:02 +0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Сергей Цайбель on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
